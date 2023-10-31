ade8a69acfea6c15155b4bbc09f10e6127ff12bc
Thomas Neumann <neumann@in.tum.de>
2023-10-31 16:26:32 +0100

    implement a result renderer

---
+++ sql/renderer.sql
@@ -0,0 +1,19 @@
+-- provide test results here
+with testresults as (select index as test, true as result from generate_series(1,260) s(index))
+-- render the result
+select case when state = 1048575 then image else 'XXXXXXXXXXXXXXXXXXXX' end as output from (values
+(0, '+-----------------+'),
+(1, '|......#####......|'),
+(2, '|....##.....##....|'),
+(3, '|...#.........#...|'),
+(4, '|..#..()...()..#..|'),
+(5, '|..#.....o.....#..|'),
+(6, '|.#.............#.|'),
+(7, '|..#..\...../..#..|'),
+(8, '|..#...-----...#..|'),
+(9, '|...#.........#...|'),
+(10,'|....##.....##....|'),
+(11,'|......#####......|'),
+(12,'+-----------------+')
+) image(line, image) left outer join (select line, sum(cast(power(2,ofs) as integer)) as state from (select line, test-1-20*line as ofs, test, result from (select cast((test-1)/20 as integer) as line, test, result from testresults) s) s group by line) s on image.line=s.line order by image.line;
+
