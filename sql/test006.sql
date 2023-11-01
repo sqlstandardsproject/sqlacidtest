faf90489a8e5820c4f75b9c4c1c00b63803ca907
Thomas Neumann <neumann@in.tum.de>
2023-11-01 08:00:05 +0100

    test sane behavior of numerics

---
+++ sql/test006.sql
@@ -0,0 +1,12 @@
+-- check that decimal number behave sane
+
+-- result header
+select 6 as test, s*10000000000000000 = 100000000000000 as result
+from (
+
+
+-- the query itself
+select sum(x)/10 as s from (values(0.2),(0.2),(-0.3)) s(x)
+
+
+) test
