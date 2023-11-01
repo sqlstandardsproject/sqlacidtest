63b2e959ec24b9247bb3410b2925bea17537eb61
Thomas Neumann <neumann@in.tum.de>
2023-11-01 09:57:45 +0100

    work around Postgres limitation

--- sql/test007.sql
+++ sql/test007.sql
@@ -12,5 +12,5 @@ select x, count(*) as c
 from ((select * from (values(1),(2),(2),(3),(3),(3),(4),(4),(4),(4)) s(x) except all select * from (values(1),(3),(3)) t(x)) intersect all select * from (values(2),(2),(2),(4),(3),(3)) u(x)) s
 group by x
 
-) s on (x is not distinct from a and c is not distinct from b)
+) s on (x=a and c=b)
 ) test
