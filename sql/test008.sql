a9b673fbdde5676978d12e401ec8b50e0839135f
Tim Fischer <tim.fischer@uni-tuebingen.de>
2023-11-01 09:44:29 +0100

    test string concat operator

---
+++ sql/test008.sql
@@ -0,0 +1,10 @@
+-- check that || is actually the string concat operator...
+
+-- result header
+select 8 as test, s = 'abcdef' as result
+from (
+
+-- the query itself
+select 'abc' || 'def'
+
+) s;
