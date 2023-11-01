8a3e6a4878c78c1f9208273ef13d76f8f5e40d35
Hannes Mühleisen <hannes@muehleisen.org>
2023-11-01 10:26:37 +0100

    add string minmax

--- sql/test009.sql
+++ sql/test009.sql
@@ -1,24 +1,28 @@
 -- check aggregate behavior
 -- result header
 SELECT 9 AS test,
-	su = 50001 AND 
-	mi = 20001 AND 
-	ma = 30000 AND 
+	su = 50001 AND
+	mi = 20001 AND
+	ma = 30000 AND
 	av BETWEEN 25000.4 AND 25000.6 AND
-	ct = 2 AND 
-	cs = 3 AS result
+	ct = 2 AND
+	cs = 3 AND
+	mis = 'asdf' AND
+	mas = 'asdf' AS result
 FROM (
 
 
 -- all aggregates shuold ignore NULL
 SELECT 
-	sum(x::SMALLINT) as su, -- this should not overflow nor throw an error
-	min(x::SMALLINT) as mi, -- those two should just work
-	max(x::SMALLINT) as ma,
-	avg(x::SMALLINT) as av, -- should not be an integer average
-	count(x::SMALLINT) as ct, -- NULL should be excluded here
-	count(*) as cs -- NULL should be included here
+	sum(x) as su, -- this should not overflow nor throw an error
+	min(x) as mi, -- those two should just work
+	max(x) as ma,
+	avg(x) as av, -- should not be an integer average
+	count(x) as ct, -- NULL should be excluded here
+	count(*) as cs, -- NULL should be included here
+	min('asdf') as mis, -- min/max should work on strings
+	max('asdf') as mas
 
-FROM (VALUES(30000), (20001), (NULL)) s(x)
+FROM (VALUES(CAST(30000 AS SMALLINT)), (CAST(20001 AS SMALLINT)), (NULL)) s(x)
 
 ) test
