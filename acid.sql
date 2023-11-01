c72a5ffaae8312153b4aca7c118264bbcd535c25
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-11-01 12:02:53 +0100

    Stitch and move

--- acid.sql
+++ acid.sql
@@ -146,14 +146,41 @@ UNION ALL
 SELECT 12 AS test, (SELECT SUM(x))=126 AS result
 FROM (VALUES (42), (84)) AS t(x)
 UNION ALL
+-- check that recursive queries work
+
+-- result header
+select 13 as test, (state='924875136138624795765391842546713928812469357397582614651238479489157263273946581') as result
+from (
+
+
+-- the query itself
+with recursive
+   digits(value,ch) as (values(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9')),
+   sudoku(state, next) as
+   (select state, position(' ' in state) as next
+    from (select '9     1 6   62 79   5  1   54     2 81  6935739 5826 46      7   915   32 3 46   ') s(state)
+   union all
+   select state, position(' ' in state) as next
+   from (select substring(state from 1 for next-1) || try || substring(state from next+1) as state
+         from sudoku, (select ch as try from digits) g
+         where next > 0 and 
+         not exists(select 1 from (select value as pos from digits) s
+                    where try = substring(state from cast(floor((next-1)/9) as integer)*9+pos for 1)
+                    or    try = substring(state from mod((next-1),9)+9*pos-8 for 1)
+                    or    try = substring(state from mod(cast(floor((next-1)/3) as integer),3)*3+cast(floor((next-1)/27) as integer)*27+pos+cast(floor((pos-1)/3) as integer)*6 for 1))
+        ) c)
+select state from sudoku where next=0
+
+) test
+UNION ALL
 -- check that conjunctions correctly handle NULL values
-SELECT 13 AS test,
+SELECT 14 AS test,
     NULL OR x>0
     AND
     NOT (NULL AND x<0) AS result
 FROM (VALUES (42)) AS t(x)
 UNION ALL
-select index as test, true as result from generate_series(14,260) s(index) 
+select index as test, true as result from generate_series(15,260) s(index) 
 )
 -- render the result
 select case when state = 1048575 then image else 'XXXXXXXXXXXXXXXXXXXX' end as output from (values
--- sql/test014.sql
+++ sql/test014.sql
@@ -1,5 +1,5 @@
 -- check that conjunctions correctly handle NULL values
-SELECT 13 AS test,
+SELECT 14 AS test,
     NULL OR x>0
     AND
     NOT (NULL AND x<0) AS result
