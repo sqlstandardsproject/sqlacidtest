244271c3257223ce973312be52886d69b14303e7
Hannes Mühleisen <hannes@muehleisen.org>
2023-11-01 10:09:51 +0100

    mc test 9

--- sql/test002.sql
+++ sql/test002.sql
@@ -1,1 +1,3 @@
-SELECT 2 as test, 4.8::INTEGER = 5 as result
+-- test that casting to integer rounds and does not truncate
+
+SELECT 2 as test, CAST (4.8 AS INTEGER) = 5 AND CAST(4.2 AS INTEGER) = 4 as result
--- sql/test009.sql
+++ sql/test009.sql
@@ -1,10 +1,10 @@
 -- check aggregate behavior
 -- result header
-SELECT 42 AS test, *, 
+SELECT 9 AS test,
 	su = 50001 AND 
 	mi = 20001 AND 
 	ma = 30000 AND 
-	av BETWEEN 25000.4 AND 25000.6,
+	av BETWEEN 25000.4 AND 25000.6 AND
 	ct = 2 AND 
 	cs = 3 AS result
 FROM (
