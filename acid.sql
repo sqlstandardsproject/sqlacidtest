d81acd21b376954bae2e836fd7f9e99db028881a
Moritz Eyssen <moritz.ey.com@posteo.de>
2023-11-02 16:48:10 +0000

    blab

--- acid.sql
+++ acid.sql
@@ -493,8 +493,8 @@ select state from sudoku where next=0
 ) testcase(result) UNION ALL select 26 as test, result from (
 -- tests/convention/test026.sql
 -- Test that an alias from the select clause can be used in the order by clause
--- Should be standard compliant, but I cannot find the section in the
--- standard that says so explicitly.
+-- Should be standard compliant (CD 9075-2:201?(E) 7.16 <query expression>), 
+-- but I cannot find the section that says so explicitly.
 select case when (true
   and (select a as b from (values (1)) t(a) order by b) = 1
 ) then 'T' else 'F' end from (values (1)) AS t
