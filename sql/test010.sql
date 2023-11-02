33d2bf6e8ba9381172123a8a4df29c8b10c9a4c6
Thomas Neumann <neumann@in.tum.de>
2023-11-02 10:12:37 +0100

    use varchar instead of text

--- sql/test010.sql
+++ sql/test010.sql
@@ -2,4 +2,4 @@
 
 SELECT case when CAST('123' AS char(4)) =  CAST('123 ' AS char(4))
          AND
-       CAST('123' AS text)    <> CAST('123 ' AS text) then 'T' else 'F' end AS result
+       CAST('123' AS varchar(10))    <> CAST('123 ' AS varchar(10)) then 'T' else 'F' end AS result
