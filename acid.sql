54fc15b3c92960285410a9d389dcfb6fab179a7d
Moritz Eyssen <moritz.ey.com@posteo.de>
2023-11-02 10:19:13 +0000

    addacid

--- acid.sql
+++ acid.sql
@@ -243,7 +243,38 @@ SELECT
 
 SELECT CASE WHEN T.HeLlO=t.hello THEN 'T' ELSE 'F' END AS result
 FROM (SELECT 42 AS hello) AS t
-) testcase(result) UNION ALL select index as test, 'T' as result from generate_series(18,260) s(index) 
+) testcase(result) UNION ALL select 18 as test, result from (
+-- sql/test018.sql
+select case when (1=1      
+  AND 0 BETWEEN -1 AND 1
+  AND 0.99 BETWEEN 0 AND 1    
+                  
+  AND 0 BETWEEN 0 AND 0
+  AND 1 BETWEEN 0 AND 1
+  AND 0 BETWEEN 0 AND 1 
+                  
+  AND -0 BETWEEN -0 AND +0
+  AND -0.00 BETWEEN -0.00 AND +0.00
+       
+  AND 1.00 BETWEEN 0 AND 1.00           
+  AND NOT 1.01 BETWEEN 0 AND 1
+  
+  AND 'a' BETWEEN 'a' AND 'b'
+  AND 'ab' BETWEEN 'a' AND 'b'
+  AND 'b' BETWEEN 'a' AND 'b'
+  AND NOT 'bla' BETWEEN 'a' AND 'b'
+  
+  AND (NULL BETWEEN NULL AND NULL) IS NULL 
+  AND (NULL BETWEEN 0 AND NULL) IS NULL 
+  AND (0 BETWEEN 0 AND NULL) IS NULL 
+  AND (NULL BETWEEN 0 AND 1) IS NULL 
+  
+  
+  AND TRUE BETWEEN FALSE AND TRUE
+  AND FALSE BETWEEN FALSE AND TRUE
+    
+) then 'T' else 'F' end as result;
+) testcase(result) UNION ALL select index as test, 'T' as result from generate_series(19,260) s(index) 
 )
 -- render the result
 select case when state = 1048575 then image else 'XXXXXXXXXXXXXXXXXXXX' end as output from (values
